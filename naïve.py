adcae337eeb5fb2fc1d974e8c630e1fc7db74e89
Lev Orekhov <lev.orekhov@gmail.com>
2017-01-20 23:19:30 +0500

    golang and Haskell versions were added (#7)
    * golang and Haskell versions were added
    * python implementation was added
    * README.md updated

---
+++ naïve.py
@@ -0,0 +1,18 @@
+def get_nth_prime(n):
+  j, i = 2, 0
+  while True:
+    if i == n - 1:
+      return j - 1
+
+    if is_prime(j):
+      i += 1
+    j += 1
+
+
+def is_prime(n):
+  for i in range(2, n):
+    if n % i == 0:
+      return False
+  return True
+
+print get_nth_prime(10000)
